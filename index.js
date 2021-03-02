8a7bb0116b1193ff73ab5f71fbd611fab80a928b
SergJohn <mariosergiodublin@gmail.com>
2021-03-02 10:06:51 +0000

    getting data from external API

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const http = require('http'); 
 const axios = require('axios'); 
 
+let users = []; // names of users will be stored here
+(async function getNames(){
+    try {
+        const {data} = await axios.get("https://jsonplaceholder.typicode.com/users");
+        users = data.map(user=>user.email);
+        console.log(users)
+    } catch (error) {
+        
+    }
+    
+})();
+
+// axios.get("https://jsonplaceholder.typicode.com/users")
+//     .then(({ data }) => {
+//         users = data.map(user => user.email); // get only the names of the users and store in an array
+//         console.log(users);
+//     })
+//     .catch(error=>{
+//         console.log(error);
+//     }); 
+
+
 http.createServer((req, res)=>{ 
-    res.write("Hello world \n"); 
+    res.write(users.join("\n")); 
     res.end();
 }).listen(8000); 
 
