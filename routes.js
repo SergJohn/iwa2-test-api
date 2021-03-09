a32a9f7d7e3d6118f4ed5f9eab0713dd440cb5f5
SergJohn <mariosergiodublin@gmail.com>
2021-03-09 11:08:29 +0000

    created route file

---
+++ routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+// router.get('/hello', (req, res) => {
+//     res.json({characteres: characteres});
+// });
+
+router.get('/hello/:foo/:bar', (req, res) => {
+    res.json({message: 'Hello BScBest!', data: [
+        req.params.foo,
+        req.params.bar
+    ]});
+});
+
+router.post('/hello', (req, res) => {
+    res.json({result: 'Post was sent', data: req.body});
+});
+
+module.exports = router;
